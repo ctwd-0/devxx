9f3c019ae6404cf48236ff1d828ce3e430f5ad1a
ctwd-0 <suncong10@gmail.com>
2017-12-04 15:18:43 +0800

    增添过滤模式

--- js/model_viewer.js
+++ js/model_viewer.js
@@ -6,12 +6,13 @@ var directionalLight;
 
 var mouse = new THREE.Vector2();
 
-var raycaster, INTERSECTED;
+var raycaster;
 
 var metadata = {};
 var lookup = {};
 var group_info = {};
 var names = {};
+var ids = {};
 var group_count, root_groups = [];
 var meta_ready_count = 0;
 
@@ -76,8 +77,142 @@ function on_names(file_names) {
 			uuid: pts[1],
 			name: pts[2],
 		};
+		if(ids[pts[2]] === undefined) {
+			ids[pts[2]] = [];
+		}
+		ids[pts[2]].push({
+			uuid: pts[1],
+			id: pts[0],
+		});
 	});
 	on_meta_index_group_ready();
+}
+
+function lookup_object_id(name) {
+	if(ids[name] === undefined) {
+		return -1;
+	} else if(ids[name].length != 1) {
+		return -2;
+	} else {
+		return ids[name][0].id;
+	}
+}
+
+function triger_direct_to_object(id) {
+	var object_name = 'g_-1_o_' + id;
+	sub_group_id = check_sub_group('-1', id);
+	if(sub_group_id === -1 || sub_group_id === -2) {
+		alert('这个构件未被任何一个群组包含，请检查');
+	} else {
+		var group_name = 'g_-1_g_' + sub_group_id;
+		var sub_group = root_meshes[group_name];
+		var object = {};
+		for(var i in sub_group.children) {
+			if(sub_group.children[i].name === object_name) {
+				object = sub_group.children[i];
+			}
+		}
+		if(object !== {}) {
+			direct_to_object(sub_group, object);
+		}
+	}
+}
+var filter_data = null;
+
+function triger_clear_filter_data() {
+	if(filter_data === null) {
+		return;
+	}
+	for(var key in current_meshes) {
+		if(current_meshes[key] instanceof THREE.Object3D) {
+			current_meshes[key].material.transparent = false;
+			current_meshes[key].material.opacity = 1.0;
+		}
+	}
+
+	for(var type in filter_data) {
+		for(var key in filter_data[type].meshes) {
+			var mesh = filter_data[type].meshes[key].mesh;
+			mesh.material = mesh.bak_material;
+		}
+	}
+	
+	filter_data = null;
+}
+
+function triger_filter_objects_fill_data(data) {
+	for(var key in data) {
+		data[key].ids = [];
+		data[key].meshes = [];
+		for(var i in data[key].names) {
+			id = lookup_object_id(data[key].names[i]);
+			if(id !== -1 && id !== -2) {
+				if(data[key].ids.indexOf(id) === -1) {
+					sub_group_id = check_sub_group('-1', id);
+					if (sub_group_id === -2) {
+
+					} else if(sub_group_id === -1) {
+						data[key].ids.push(id);
+						mesh_name = current_model + '_o_' + id;
+						data[key].meshes.push({
+							mesh_name:mesh_name,
+							mesh: current_meshes[mesh_name],
+						}); 
+					} else {
+						data[key].ids.push(id);
+						mesh_name = current_model + '_o_' + id;
+						group_name = current_model + '_g_' + sub_group_id;
+						data[key].meshes.push({
+							mesh_name:mesh_name,
+							group_name:group_name,
+							group: current_meshes[group_name],
+							mesh: current_meshes[group_name].meshes[mesh_name],
+						}); 
+					}
+				}
+			}
+		}
+	}
+}
+
+function triger_filter_objects_set_meshes(data) {
+	filter_data = data;
+	for(var key in current_meshes) {
+		if(current_meshes[key] instanceof THREE.Object3D) {
+			current_meshes[key].material.transparent = true;
+			current_meshes[key].material.opacity = 0.2;
+		}
+	}
+
+	for(var type in data) {
+		for(var key in data[type].meshes) {
+			var mesh = data[type].meshes[key].mesh;
+			mesh.bak_material = mesh.material;
+			mesh.material = mesh.bak_material.clone();
+			mesh.material.transparent = false;
+			mesh.material.opacity = 1.0;
+			mesh.material.color = data[type].color;
+		}
+	}
+}
+
+function triger_filter_objects(data) {
+	if(filter_data !== null) {
+		triger_clear_filter_data();
+	}
+	
+	if(current_model !== 'g_-1') {
+		alert('暂时只支持在顶层过滤');
+		return ;
+	}
+
+	if(data === {}) {
+		return;
+	}
+
+	triger_filter_objects_fill_data(data);
+	
+	triger_filter_objects_set_meshes(data);
 }
 
 function on_meta_index_group_ready() {
@@ -133,10 +268,12 @@ function load_xbjs_and_add_to_scene(mesh_to_load) {
 						root_meshes[sub_group_name] = new THREE.Group();
 						root_meshes[sub_group_name].name = sub_group_name;
 						root_meshes[sub_group_name].material = mesh.material;
+						root_meshes[sub_group_name].meshes = {};
+						scene.add(root_meshes[sub_group_name]);
 					}
 					root_meshes[sub_group_name].add(mesh);
+					root_meshes[sub_group_name].meshes[mesh.name] = mesh;
 					mesh.material = root_meshes[sub_group_name].material;
-					scene.add(root_meshes[sub_group_name]);
 				} else {
 					root_meshes[mesh.name] = mesh;
 					scene.add( mesh );
@@ -235,7 +372,9 @@ var STATE = {
 	NONE: -1, 
 	CHANGE_FOCOUS: 1,
 	BACK_TO_PARENT: 2,
-	ROTATE_TO_DIR: 3};
+	ROTATE_TO_DIR: 3,
+	DIRECT_TO_OBJECT: 4,
+};
 var target0, target1, position0, position1, up0, up1;
 var start_time;
 var change_focous_duration = 500;
@@ -307,7 +446,7 @@ function prepare_hr_meshes(name, box) {
 
 function change_meshes_and_backup(name) {
 	if(state === STATE.NONE) {
-		if(current_model === 'g_' + root_groups[0]) {
+		if(current_model === 'g_-1') {
 			low_resolution_meshes = root_meshes;
 		} else {
 			low_resolution_meshes = current_meshes;
@@ -318,7 +457,12 @@ function change_meshes_and_backup(name) {
 		});
 
 		for(var key in low_resolution_meshes) {
+			if(low_resolution_meshes[key] instanceof THREE.Object3D)
 			scene.remove(low_resolution_meshes[key]);
+		}
+		if(direct_object !== null) {
+			direct_object.material = direct_object.bak_material;
+			scene.remove(direct_object);
 		}
 		for(var key in high_resolution_meshes) {
 			if(high_resolution_meshes[key] instanceof THREE.Object3D) {
@@ -329,6 +473,7 @@ function change_meshes_and_backup(name) {
 		current_model = name;
 		waiting_for_mesh = false;
 		clear_transparency_and_color(low_resolution_meshes);
+
 	} else {
 		hr_mesh_ready_unchanged = {
 			name:name,
@@ -352,6 +497,7 @@ function rotate_to_certain_dir(dir) {
 }
 
 function change_focous(mesh) {
+	triger_clear_filter_data();
 	name = mesh.name;
 	info = name.split('_');
 	if(info.length == 2) {
@@ -373,6 +519,31 @@ function change_focous(mesh) {
 		prepare_hr_meshes(name, box);
 		waiting_for_mesh = true;
 	}
+}
+
+var direct_object = null;
+
+function direct_to_object(sub_group, object) {
+	direct_object = object;
+	name = object.name;
+	object.bak_material = object.material;
+	object.material = object.bak_material.clone();
+	info = name.split('_');
+
+	box = new THREE.Box3();
+	box.setFromObject(object);
+
+	target1 = box.getCenter();
+	target0 = controls.target.clone();
+	position0 = camera.position.clone();
+	size = box.getSize().length();
+	direction = position0.clone().sub(target0).normalize();
+	position1 = target1.clone().add(direction.setLength(size));
+	controls = null;
+	start_time = new Date().getTime();
+	state = STATE.DIRECT_TO_OBJECT;
+	prepare_hr_meshes(name, box);
+	waiting_for_mesh = true;
 }
 
 function back_to_parent() {
@@ -401,10 +572,6 @@ function back_to_parent() {
 	} else {
 		return;
 	}
-}
-
-function force_select(name) {
-
 }
 
 function on_double_click(event) {
@@ -546,159 +713,236 @@ function render() {
 		renderer.render( scene, camera );
 
 	} else if(state == STATE.CHANGE_FOCOUS) {
-		time = new Date().getTime() - start_time;
-		if(time > change_focous_duration) {
-			camera.position.copy(position1);
-			camera.lookAt(target1);
-
-			controls = new THREE.TrackballControls( camera, container );
-			controls.staticMoving = true;
-			controls.target = target1.clone(); 
-			controls.update();
-
-			dir = position1.clone().sub(target1).normalize();
-			directionalLight.position.copy(dir);
-			
-			state = STATE.NONE;
-			
-			for(var key in current_meshes) {
-				if(current_meshes[key].material) {
-					current_meshes[key].material.transparent = true;
-					current_meshes[key].material.opacity = 0;
-				}
-			}
-			if(current_meshes[focous_name] !== undefined) {
-				if(current_meshes[focous_name].material) {
-					current_meshes[focous_name].material.transparent = false;
-					current_meshes[focous_name].material.opacity = 1;
-				}
-			}
-
-
-			if(hr_mesh_ready_unchanged.name !== undefined) {
-				change_meshes_and_backup(hr_mesh_ready_unchanged.name);
-				hr_mesh_ready_unchanged = {};
-				waiting_for_mesh = false;
-			} else {
-				for(var key in low_resolution_meshes) {
-					if(low_resolution_meshes[key] instanceof THREE.Object3D 
-						&& key !== focous_name) {
-						scene.remove(low_resolution_meshes[key]);
-					}
-				}
-			}
-			focous_name = '';
-			renderer.render( scene, camera );
-
-		} else {
-			scaler = time / change_focous_duration;
-			var target = target0.clone().multiplyScalar(1 - scaler);
-			target.add(target1.clone().multiplyScalar(scaler));
-			var position = position0.clone().multiplyScalar(1 - scaler);
-			position.add(position1.clone().multiplyScalar(scaler));
-			camera.position.copy(position);
-			camera.lookAt(target);
-			
-			dir = position.clone().sub(target).normalize();
-			directionalLight.position.copy(dir);
-
-			for(var key in current_meshes) {
-				if(current_meshes[key].material) {
-					current_meshes[key].material.transparent = true;
-					current_meshes[key].material.opacity = 1 - scaler;
-				}
-			}
-			if(current_meshes[focous_name] !== undefined) {
-				if(current_meshes[focous_name].material) {
-					current_meshes[focous_name].material.transparent = false;
-					current_meshes[focous_name].material.opacity = 1;
-				}
-			}
-
-			renderer.render( scene, camera );
-		}
+		render_change_focous();
 	} else if(state == STATE.BACK_TO_PARENT){
-		time = new Date().getTime() - start_time;
-		if(time > back_to_parent_duration) {
-			camera.position.copy(position1);
-			camera.lookAt(target1);
-
-			controls = new THREE.TrackballControls( camera, container );
-			controls.staticMoving = true;
-			controls.target = target1.clone(); 
-			controls.update();
-
-			dir = position1.clone().sub(target1).normalize();
-			directionalLight.position.copy(dir);
-			
-			state = STATE.NONE;
-			
-			for(var key in current_meshes) {
-				scene.remove(current_meshes[key]);
-			}
-
-			current_model = parent_scene.model_name;
-			current_meshes = parent_scene.model_meshes;
-			parent_scene = {};
-			clear_transparency_and_color(current_meshes);
-
-			renderer.render( scene, camera );
-		} else {
-			scaler = time / change_focous_duration;
-			var target = target0.clone().multiplyScalar(1 - scaler);
-			target.add(target1.clone().multiplyScalar(scaler));
-			var position = position0.clone().multiplyScalar(1 - scaler);
-			position.add(position1.clone().multiplyScalar(scaler));
-			camera.position.copy(position);
-			camera.lookAt(target);
-			
-			dir = position.clone().sub(target).normalize();
-			directionalLight.position.copy(dir);
-
-			for(var key in parent_scene.model_meshes) {
-				if(parent_scene.model_meshes[key].material) {
-					parent_scene.model_meshes[key].material.transparent = true;
-					parent_scene.model_meshes[key].material.opacity = scaler;
-				}
-			}
-
-			renderer.render( scene, camera );
-		}
+		render_back_to_parent();
 	} else if(state == STATE.ROTATE_TO_DIR){
-		time = new Date().getTime() - start_time;
-		if(time > rotate_to_dir_duration) {
-			camera.position.copy(position1);
-			camera.up.copy(up1);
-			camera.lookAt(target1);
-
-			controls = new THREE.TrackballControls( camera, container );
-			controls.staticMoving = true;
-			controls.target = target1.clone(); 
-			controls.update();
-
-			dir = position1.clone().sub(target1).normalize();
-			directionalLight.position.copy(dir);
-			
-			state = STATE.NONE;
-			renderer.render( scene, camera );
-		} else {
-			scaler = time / change_focous_duration;
-			var target = target0.clone().multiplyScalar(1 - scaler);
-			target.add(target1.clone().multiplyScalar(scaler));
-			var position = position0.clone().multiplyScalar(1 - scaler);
-			position.add(position1.clone().multiplyScalar(scaler));
-			var up = up0.clone().multiplyScalar(1 - scaler);
-			up.add(up1.clone().multiplyScalar(scaler));
-			camera.position.copy(position);
-			camera.up.copy(up);
-			camera.lookAt(target);
-			
-			dir = position.clone().sub(target).normalize();
-			directionalLight.position.copy(dir);
-
-			renderer.render( scene, camera );
-		}
+		render_rotate_to_dir();
+	} else if(state == STATE.DIRECT_TO_OBJECT){
+		render_direct_to_object();
 	} else {
 		//
+	}
+}
+
+function render_change_focous() {
+	time = new Date().getTime() - start_time;
+	if(time > change_focous_duration) {
+		camera.position.copy(position1);
+		camera.lookAt(target1);
+
+		controls = new THREE.TrackballControls( camera, container );
+		controls.staticMoving = true;
+		controls.target = target1.clone(); 
+		controls.update();
+
+		dir = position1.clone().sub(target1).normalize();
+		directionalLight.position.copy(dir);
+		
+		state = STATE.NONE;
+		
+		for(var key in current_meshes) {
+			if(current_meshes[key] instanceof THREE.Object3D) {
+				current_meshes[key].material.transparent = true;
+				current_meshes[key].material.opacity = 0;
+			}
+		}
+
+		if(current_meshes[focous_name] !== undefined) {
+			if(current_meshes[key] instanceof THREE.Object3D) {
+				current_meshes[focous_name].material.transparent = false;
+				current_meshes[focous_name].material.opacity = 1;
+			}
+		}
+
+		if(hr_mesh_ready_unchanged.name !== undefined) {
+			change_meshes_and_backup(hr_mesh_ready_unchanged.name);
+			hr_mesh_ready_unchanged = {};
+			waiting_for_mesh = false;
+		} else {
+			// for(var key in low_resolution_meshes) {
+			// 	if(low_resolution_meshes[key] instanceof THREE.Object3D 
+			// 		&& key !== focous_name) {
+			// 		scene.remove(low_resolution_meshes[key]);
+			// 	}
+			// }
+		}
+
+		focous_name = '';
+		renderer.render( scene, camera );
+
+	} else {
+		scaler = time / change_focous_duration;
+		var target = target0.clone().multiplyScalar(1 - scaler);
+		target.add(target1.clone().multiplyScalar(scaler));
+		var position = position0.clone().multiplyScalar(1 - scaler);
+		position.add(position1.clone().multiplyScalar(scaler));
+		camera.position.copy(position);
+		camera.lookAt(target);
+		
+		dir = position.clone().sub(target).normalize();
+		directionalLight.position.copy(dir);
+		
+		for(var key in current_meshes) {
+			if(current_meshes[key] instanceof THREE.Object3D) {
+				current_meshes[key].material.transparent = true;
+				current_meshes[key].material.opacity = 0;
+			}
+		}
+
+		for(var key in current_meshes) {
+			if(current_meshes[key] instanceof THREE.Object3D) {
+				current_meshes[key].material.transparent = true;
+				current_meshes[key].material.opacity = 1 - scaler;
+			}
+		}
+		if(current_meshes[focous_name] !== undefined) {
+			if(current_meshes[key] instanceof THREE.Object3D) {
+				current_meshes[focous_name].material.transparent = false;
+				current_meshes[focous_name].material.opacity = 1;
+			}
+		}
+
+		renderer.render( scene, camera );
+	}
+}
+
+function render_back_to_parent() {
+	time = new Date().getTime() - start_time;
+	if(time > back_to_parent_duration) {
+		camera.position.copy(position1);
+		camera.lookAt(target1);
+
+		controls = new THREE.TrackballControls( camera, container );
+		controls.staticMoving = true;
+		controls.target = target1.clone(); 
+		controls.update();
+
+		dir = position1.clone().sub(target1).normalize();
+		directionalLight.position.copy(dir);
+		
+		state = STATE.NONE;
+		
+		for(var key in current_meshes) {
+			scene.remove(current_meshes[key]);
+		}
+
+		current_model = parent_scene.model_name;
+		current_meshes = parent_scene.model_meshes;
+		parent_scene = {};
+		clear_transparency_and_color(current_meshes);
+
+		renderer.render( scene, camera );
+	} else {
+		scaler = time / change_focous_duration;
+		var target = target0.clone().multiplyScalar(1 - scaler);
+		target.add(target1.clone().multiplyScalar(scaler));
+		var position = position0.clone().multiplyScalar(1 - scaler);
+		position.add(position1.clone().multiplyScalar(scaler));
+		camera.position.copy(position);
+		camera.lookAt(target);
+		
+		dir = position.clone().sub(target).normalize();
+		directionalLight.position.copy(dir);
+
+		for(var key in parent_scene.model_meshes) {
+			if(parent_scene.model_meshes[key].material) {
+				parent_scene.model_meshes[key].material.transparent = true;
+				parent_scene.model_meshes[key].material.opacity = scaler;
+			}
+		}
+
+		renderer.render( scene, camera );
+	}
+}
+
+function render_rotate_to_dir() {
+	time = new Date().getTime() - start_time;
+	if(time > rotate_to_dir_duration) {
+		camera.position.copy(position1);
+		camera.up.copy(up1);
+		camera.lookAt(target1);
+
+		controls = new THREE.TrackballControls( camera, container );
+		controls.staticMoving = true;
+		controls.target = target1.clone(); 
+		controls.update();
+
+		dir = position1.clone().sub(target1).normalize();
+		directionalLight.position.copy(dir);
+		
+		state = STATE.NONE;
+		renderer.render( scene, camera );
+	} else {
+		scaler = time / change_focous_duration;
+		var target = target0.clone().multiplyScalar(1 - scaler);
+		target.add(target1.clone().multiplyScalar(scaler));
+		var position = position0.clone().multiplyScalar(1 - scaler);
+		position.add(position1.clone().multiplyScalar(scaler));
+		var up = up0.clone().multiplyScalar(1 - scaler);
+		up.add(up1.clone().multiplyScalar(scaler));
+		camera.position.copy(position);
+		camera.up.copy(up);
+		camera.lookAt(target);
+		
+		dir = position.clone().sub(target).normalize();
+		directionalLight.position.copy(dir);
+
+		renderer.render( scene, camera );
+	}
+}
+
+function render_direct_to_object() {
+	time = new Date().getTime() - start_time;
+	if(time > change_focous_duration) {
+		camera.position.copy(position1);
+		camera.lookAt(target1);
+
+		controls = new THREE.TrackballControls( camera, container );
+		controls.staticMoving = true;
+		controls.target = target1.clone(); 
+		controls.update();
+
+		dir = position1.clone().sub(target1).normalize();
+		directionalLight.position.copy(dir);
+		
+		state = STATE.NONE;
+
+
+		if(hr_mesh_ready_unchanged.name !== undefined) {
+			change_meshes_and_backup(hr_mesh_ready_unchanged.name);
+			hr_mesh_ready_unchanged = {};
+			waiting_for_mesh = false;
+		} else {
+			// for(var key in low_resolution_meshes) {
+			// 	if(low_resolution_meshes[key] instanceof THREE.Object3D ) {
+			// 		scene.remove(low_resolution_meshes[key]);
+			// 	}
+			// }
+			// scene.add(direct_object);
+		}
+
+		renderer.render( scene, camera );
+
+	} else {
+		scaler = time / change_focous_duration;
+		var target = target0.clone().multiplyScalar(1 - scaler);
+		target.add(target1.clone().multiplyScalar(scaler));
+		var position = position0.clone().multiplyScalar(1 - scaler);
+		position.add(position1.clone().multiplyScalar(scaler));
+		camera.position.copy(position);
+		camera.lookAt(target);
+		
+		dir = position.clone().sub(target).normalize();
+		directionalLight.position.copy(dir);
+
+		for(var key in current_meshes) {
+			if(current_meshes[key] instanceof THREE.Object3D) {
+				current_meshes[key].material.transparent = true;
+				current_meshes[key].material.opacity = 1 - scaler;
+			}
+		}
+
+		renderer.render( scene, camera );
 	}
 }
